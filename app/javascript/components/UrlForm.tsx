ca79a2598d370f2c45f22d1699b8538f3963d376
Jack Ford <jford8820@gmail.com>
2021-08-05 17:31:59 -0400

    UrlForm finally done. Still doesnt update top 100 after creating a URL, might be difficult with async. Still no CSS, think antd will come next

--- app/javascript/components/UrlForm.tsx
+++ app/javascript/components/UrlForm.tsx
@@ -1,5 +1,66 @@
 import * as React from 'react';
 
-const UrlForm = () => <div></div>;
+const { useState } = React;
+
+// Ephemeral state for questions that haven't been added yet
+const useForm = () => {
+  const [formUrl, setUrl] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    if (event.target instanceof HTMLInputElement) {
+      setUrl(url => event.target.value);
+    }
+  };
+
+  const handleKeyUp = (event: React.KeyboardEvent<HTMLInputElement>): void => {
+    if (event.currentTarget instanceof HTMLInputElement) {
+      const { code, currentTarget } = event;
+  
+      if (code === 'Enter') {
+        fetch('http://localhost:3000/', {
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ full_url: currentTarget.value })
+        }).then(
+          (response) => response.json(),
+        ).then((data) => {
+          console.log('Created ShortUrl:', data.short_code);
+          setUrl(url => '');
+        });
+      }
+    }
+  };
+
+  return {
+    formUrl,
+    handleChange,
+    handleKeyUp,
+  };
+};
+
+const UrlForm = () => {
+  const { formUrl, handleChange, handleKeyUp } = useForm();
+
+  return (
+    <div className='form-container'>
+      {/* TODO Flex 0 auto */}
+      <div className='flex'>
+        <input
+          id='url'
+          name='url'
+          type='text'
+          className='url-input form-control'
+          placeholder='Type a URL to shorten'
+          aria-label='Url'
+          value={formUrl}
+          onChange={handleChange}
+          onKeyUp={handleKeyUp}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default UrlForm;
